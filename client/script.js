79609fb3bf6de7cca6b0d89f7466240398412a29
greggaka <greggakamine@gmail.com>
2023-02-01 22:48:39 -1000

    updated server url

--- client/script.js
+++ client/script.js
@@ -85,7 +85,7 @@ const handleSubmit = async (e) => {
   // messageDiv.innerHTML = "..."
   loader(messageDiv)
 
-  const response = await fetch('https://chatgpt-clone-pg6s.onrender.com', {
+  const response = await fetch('https://chatgpt-clone-7ls7.onrender.com/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
